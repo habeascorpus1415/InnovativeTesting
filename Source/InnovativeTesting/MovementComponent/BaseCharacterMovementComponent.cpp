a71d92fcc30540f5815fec99003884572e8109d5
shenok <35558582+habeascorpus1415@users.noreply.github.com>
2021-01-21 18:23:57 +0200

    movementcomponent
    add movement component for client fix

--- Source/InnovativeTesting/MovementComponent/BaseCharacterMovementComponent.cpp
+++ Source/InnovativeTesting/MovementComponent/BaseCharacterMovementComponent.cpp
@@ -21,13 +21,29 @@ void UBaseCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
 {
 	Super::UpdateFromCompressedFlags(Flags);
 
+	//The Flags parameter contains the compressed input flags that are stored in the saved move.
+	//UpdateFromCompressed flags simply copies the flags from the saved move into the movement component.
+	//It basically just resets the movement component to the state when the move was made so it can simulate from there.
+	RequestToStartSprinting = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+
+	RequestToStartADS = (Flags & FSavedMove_Character::FLAG_Custom_1) != 0;
+
 }
 
 FNetworkPredictionData_Client * UBaseCharacterMovementComponent::GetPredictionData_Client() const
 {
 	check(PawnOwner != NULL);
 
-	return nullptr;
+	if (!ClientPredictionData)
+	{
+		UBaseCharacterMovementComponent * MutableThis = const_cast<UBaseCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FBaseNetworkPredictionData_Client(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+
+	return ClientPredictionData;
 }
 
 void UBaseCharacterMovementComponent::StopSprinting()
@@ -38,4 +54,77 @@ void UBaseCharacterMovementComponent::StopSprinting()
 void UBaseCharacterMovementComponent::StartSprinting(float NewSprintSpeedMultiplier)
 {
 	SprintSpeedMultiplier = NewSprintSpeedMultiplier;
+}
+
+void UBaseCharacterMovementComponent::FBaseSavedMove::Clear()
+{
+	Super::Clear();
+
+	SavedRequestToStartSprinting = false;
+	SavedRequestToStartADS = false;
+}
+
+uint8 UBaseCharacterMovementComponent::FBaseSavedMove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (SavedRequestToStartSprinting)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	if (SavedRequestToStartADS)
+	{
+		Result |= FLAG_Custom_1;
+	}
+
+	return Result;
+}
+
+bool UBaseCharacterMovementComponent::FBaseSavedMove::CanCombineWith(const FSavedMovePtr & NewMove, ACharacter * Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+	if (SavedRequestToStartSprinting != ((FBaseSavedMove*)&NewMove)->SavedRequestToStartSprinting)
+	{
+		return false;
+	}
+
+	if (SavedRequestToStartADS != ((FBaseSavedMove*)&NewMove)->SavedRequestToStartADS)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UBaseCharacterMovementComponent::FBaseSavedMove::SetMoveFor(ACharacter * Character, float InDeltaTime, FVector const & NewAccel, FNetworkPredictionData_Client_Character & ClientData)
+{
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	UBaseCharacterMovementComponent* CharacterMovement = Cast<UBaseCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+		SavedRequestToStartSprinting = CharacterMovement->RequestToStartSprinting;
+		SavedRequestToStartADS = CharacterMovement->RequestToStartADS;
+	}
+}
+
+void UBaseCharacterMovementComponent::FBaseSavedMove::PrepMoveFor(ACharacter * Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UBaseCharacterMovementComponent* CharacterMovement = Cast<UBaseCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+	}
+}
+
+UBaseCharacterMovementComponent::FBaseNetworkPredictionData_Client::FBaseNetworkPredictionData_Client(const UCharacterMovementComponent & ClientMovement) : Super(ClientMovement)
+{
+	//
+}
+
+FSavedMovePtr UBaseCharacterMovementComponent::FBaseNetworkPredictionData_Client::AllocateNewMove()
+{
+	return FSavedMovePtr(new FBaseSavedMove());
 }
--- Source/InnovativeTesting/MovementComponent/BaseCharacterMovementComponent.h
+++ Source/InnovativeTesting/MovementComponent/BaseCharacterMovementComponent.h
@@ -14,6 +14,46 @@ class INNOVATIVETESTING_API UBaseCharacterMovementComponent : public UCharacterM
 {
 	GENERATED_BODY()
 
+
+	class FBaseSavedMove : public FSavedMove_Character
+	{
+	public:
+
+		typedef FSavedMove_Character Super;
+
+		///@brief Resets all saved variables.
+		virtual void Clear() override;
+
+		///@brief Store input commands in the compressed flags.
+		virtual uint8 GetCompressedFlags() const override;
+
+		///@brief This is used to check whether or not two moves can be combined into one.
+		///Basically you just check to make sure that the saved variables are the same.
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const override;
+
+		///@brief Sets up the move before sending it to the server. 
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+		///@brief Sets variables on character movement component before making a predictive correction.
+		virtual void PrepMoveFor(class ACharacter* Character) override;
+
+		// Sprint
+		uint8 SavedRequestToStartSprinting : 1;
+
+		// Aim Down Sights
+		uint8 SavedRequestToStartADS : 1;
+	};
+
+	class FBaseNetworkPredictionData_Client : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FBaseNetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		///@brief Allocates a new copy of our custom saved move
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
 private:
 
 	/* private: UPROPERTY LIST */
@@ -54,6 +94,10 @@ private:
 public:
 
 	/* public: VARIABLE LIST */
+
+	uint8 RequestToStartSprinting : 1;
+
+	uint8 RequestToStartADS : 1;
 
 protected:
 
